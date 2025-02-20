78f37d1201470f058fa24e2639b9ba70e4b46f2f
Anwaar <anwaar.zahran@intalio.com>
2025-02-20 20:12:27 +0200

    updated

--- src/app/Modules/landing/transfer-modal/transfer-modal.component.ts
+++ src/app/Modules/landing/transfer-modal/transfer-modal.component.ts
@@ -20,13 +20,8 @@ interface User {
   id: number;
   name: string;
 }
-
-interface StructureUsers {
-  structureName: string,
-  userName: string
-}
 interface TransferRow {
-
+  
   purposeId: number | null; // Allow null
   priorityId: number | null;
   dueDate: string | null;
@@ -34,17 +29,16 @@ interface TransferRow {
   isPrivate: boolean;
   cced: boolean;
   followUp: boolean;
-  toStructureId: number | null;
-  toUserId: number | null;
+  toStructureId:number|null;
+  toUserId:number|null;
   name: string | null;
   PrivateInstruction: boolean;
   FromStructureId: boolean;
-  ParentTransferId: number | null;
+  ParentTransferId:number|null;
   IsStructure: true,
-  DocumentId: number | null;
-  DocumentPrivacyId: number | null;
+  DocumentId: number|null;
+  DocumentPrivacyId:number|null;
   showValidationError?: boolean;
-  selectedToUsers?: StructureUsers[];
 }
 
 @Component({
@@ -53,7 +47,7 @@ interface TransferRow {
     CommonModule, MatDialogModule, NgSelectModule,
     MatDatepickerModule,
     MatInputModule,
-    MatNativeDateModule, FormsModule, ToasterComponent, TranslateModule, AddressBookComponent],
+    MatNativeDateModule, FormsModule,ToasterComponent,TranslateModule],
   templateUrl: './transfer-modal.component.html',
   styleUrl: './transfer-modal.component.scss'
 })
@@ -72,11 +66,10 @@ export class TransferModalComponent implements OnInit {
   showAddressBook: boolean = false;
   selectedUserId: any;
   maxUsers = 50;
-  documentId: any;
-  documentPrivacyId: any;
-  fromStructureId: any;
-  selectedToUsers: StructureUsers[] = []
-  parentTransferId: any;
+  documentId:any;
+  documentPrivacyId:any;
+  fromStructureId:any;
+  parentTransferId:any;
   rows = [
     this.createEmptyRow() // Initialize with one empty row
   ];
@@ -91,34 +84,34 @@ export class TransferModalComponent implements OnInit {
   isPrivate: any;
   isFollowUp: any;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private authService: AuthService, private toaster: ToasterService,
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private authService: AuthService,private toaster: ToasterService,
     private router: Router, private lookupsService: LookupsService, private dialog: MatDialog, private cdr: ChangeDetectorRef,
-    private dialogRef: MatDialogRef<TransferModalComponent>, private mailService: MailsService, private translate: TranslateService) {
-    this.receivedData = data; // ✅ Initialize here to ensure it's available everywhere
-    this.documentId = this.data.documentId;
-    this.documentPrivacyId = this.data.row.privacyId;
-    this.parentTransferId = this.data.row.parentTransferId;
-    this.fromStructureId = this.data.row.toStructureId;
-  }
+    private dialogRef: MatDialogRef<TransferModalComponent>, private mailService: MailsService,private translate: TranslateService) { 
+      this.receivedData = data; // ✅ Initialize here to ensure it's available everywhere
+      this.documentId=this.data.documentId;
+      this.documentPrivacyId=this.data.row.privacyId;
+      this.parentTransferId=this.data.row.parentTransferId;
+      this.fromStructureId=this.data.row.toStructureId;
+    }
 
   ngOnInit(): void {
     //    console.log('Dialog opened with ID:', this.data.id, 'and Reference Number:', this.data.referenceNumber);
-
+   
     this.accessToken = this.authService.getToken();
-    //if (!this.accessToken) {
-    //  this.router.navigate(['/login']);
-    //  return;
-    //}
+    if (!this.accessToken) {
+      this.router.navigate(['/login']);
+      return;
+    }
     this.loadUserStructures();
   }
-
+  
   transformDataWithotSort(data: Array<{ name: string; userStructure?: Array<{ user?: { firstname?: string; lastname?: string } }> }>): string[] {
     let result: string[] = [];
-
+  
     data.forEach((structure) => {
       if (structure.name) {
         result.push(structure.name);
-
+  
         structure.userStructure?.forEach((userStruct) => {
           if (userStruct?.user?.firstname && userStruct?.user?.lastname) {
             result.push(`${structure.name}/${userStruct.user.firstname} ${userStruct.user.lastname}`);
@@ -126,21 +119,20 @@ export class TransferModalComponent implements OnInit {
         });
       }
     });
-
+  
     return result;
   }
-
-  transformDataTemp(data: Array<{
-    name: string;
-    userStructure?: Array<{ user?: { firstname?: string; lastname?: string } }>
+  transformDataTemp(data: Array<{ 
+    name: string; 
+    userStructure?: Array<{ user?: { firstname?: string; lastname?: string } }> 
   }>): string[] {
-
+    
     let result: string[] = [];
-
+  
     data.forEach((structure) => {
       if (structure.name) {
         result.push(structure.name);
-
+  
         structure.userStructure?.forEach((userStruct) => {
           if (userStruct?.user?.firstname && userStruct?.user?.lastname) {
             result.push(`${structure.name}/${userStruct.user.firstname} ${userStruct.user.lastname}`);
@@ -148,55 +140,55 @@ export class TransferModalComponent implements OnInit {
         });
       }
     });
-
+  
     // Sort the result in ascending alphabetical order
     return result.sort((a, b) => a.localeCompare(b));
   }
-  transformData(data: Array<{
-    id: number;
-    name: string;
-    userStructure?: Array<{
-      user?: {
-        id: number;
-        firstname?: string;
-        lastname?: string;
-        structureId?: number; // Include structureId
-      }
-    }>
-  }>): Array<{ id: number, name: string, isStructure: boolean, structureId?: number | undefined }> {
-
+  transformData(data: Array<{ 
+    id: number; 
+    name: string; 
+    userStructure?: Array<{ 
+        user?: { 
+            id: number; 
+            firstname?: string; 
+            lastname?: string;
+            structureId?: number; // Include structureId
+        } 
+    }> 
+}>) : Array<{ id: number, name: string, isStructure: boolean, structureId?: number | undefined }> {
+    
     let result: Array<{ id: number, name: string, isStructure: boolean, structureId?: number }> = [];
-debugger
-    data.forEach((structure) => {
-      if (structure.name) {
-        // Push the structure itself (isStructure: true, no structureId)
-        result.push({ id: structure.id, name: structure.name, isStructure: true });
 
-        structure.userStructure?.forEach((userStruct) => {
-          if (userStruct?.user?.firstname && userStruct?.user?.lastname) {
-            ;
-            result.push({
-              id: userStruct.user.id,
-              name: `${structure.name} / ${userStruct.user.firstname} ${userStruct.user.lastname}`,
-              isStructure: false,
-              structureId: structure.id ?? undefined, // Use undefined instead of null
+    data.forEach((structure) => {
+        if (structure.name) {
+            // Push the structure itself (isStructure: true, no structureId)
+            result.push({ id: structure.id, name: structure.name, isStructure: true });
+
+            structure.userStructure?.forEach((userStruct) => {
+                if (userStruct?.user?.firstname && userStruct?.user?.lastname) {
+                     ;
+                    result.push({
+                        id: userStruct.user.id,
+                        name: `${structure.name} / ${userStruct.user.firstname} ${userStruct.user.lastname}`,
+                        isStructure: false,
+                        structureId: structure.id ?? undefined, // Use undefined instead of null
+                    });
+                }
             });
-          }
-        });
-      }
+        }
     });
 
     return result;
-  }
+}
 
   //Add a new row when user changes selection
-
+  
   loadUserStructures(): void {
     this.lookupsService.getStructuredUsers(this.accessToken!).subscribe(
       (users) => {
-
-        var data = users || [];
-        this.users = this.transformData(data);
+        
+       var data= users || [];
+      this.users =this.transformData(data);
 
       },
       (error) => {
@@ -204,12 +196,12 @@ debugger
       }
     );
 
-
+    
     this.lookupsService.getPrioritiesWithDays(this.accessToken!).subscribe(
       (reponse) => {
-
+        
         this.priorities = reponse || [];
-        const defaultPriorityId = this.priorities.length > 0 ? this.priorities[0].id : null;
+       const defaultPriorityId = this.priorities.length > 0 ? this.priorities[0].id : null;
         this.rows = this.rows.map(row => ({
           ...row,
           selectedPriorityId: row.selectedPriorityId || defaultPriorityId
@@ -236,15 +228,15 @@ debugger
     if (selectedPriority && selectedPriority.numberOfDueDays !== undefined) {
       let daysToAdd = selectedPriority.numberOfDueDays; // Get dynamic days
       let currentDate = new Date();
-      currentDate.setDate(currentDate.getDate() + (daysToAdd - 1)); // Add dynamic days
-      this.rows = this.rows.map((row, i) =>
-        i === index ? { ...row, selectedDueDate: currentDate } : row
-      );
+      currentDate.setDate(currentDate.getDate() + (daysToAdd-1)); // Add dynamic days
+    this.rows = this.rows.map((row, i) =>
+    i === index ? { ...row, selectedDueDate: currentDate } : row
+  );
 
     } else {
       console.error("Priority not found or daysToAdd is undefined");
     }
-
+    
   }
   convertToNgbDateStruct(dateStr: string): NgbDateStruct | undefined {
     if (!dateStr) return undefined;
@@ -260,7 +252,7 @@ debugger
     return `${year}/${month}/${day}`;
   }
 
-  showAddressOld() {
+  showAddress() {
 
     //this.showAddressBook = !this.showAddressBook;
     const dialog = this.dialog.open(AddressBookComponent, {
@@ -271,95 +263,19 @@ debugger
 
     dialog.afterClosed().subscribe((result: User[]) => {
       if (result) {
-        debugger
         console.log('Address Book result:', result);
 
         this.selectedUsers = result.map((user: User) => ({
           ...user,
           selectedUserId: user.id
         }));
-      console.log('selected userssssssssssssss')
-        console.log(this.selectedUsers)
-        this.selectedUsers.forEach((user, index) => {
-          if (Array.isArray(user.userStructure)){
-            user.userStructure.forEach((u: any )=> {
-
-              let userName = u.user.firstname+ ' '+ u.user.lastname;
-              this.selectedToUsers.push({structureName: user.name,userName: userName})
-
-            });
-          }
-        });
-debugger
-        console.log('handle the rows')
-        if (this.rows.length > 0) {
-          this.rows = this.rows.map(row => ({
-            ...row,
-            selectedToUsers: [...this.selectedToUsers] // Ensure selectedToUsers is copied properly
-          }));
-        }
-        // if (this.selectedToUsers.length > 0) {
-        //   this.rows = this.selectedToUsers.map(user => ({
-        //     ...this.createEmptyRow(), // Create a new row structure
-        //     selectedUser: user, // Assign user details
-        //     selectedUserId: user.id // Store user ID for reference
-        //   }));
-        // }
-        
-        console.log('loopin in html on this list')
-        console.log(this.selectedToUsers)
         this.cdr.detectChanges();
       } else {
         console.log('Address Book dialog was closed without submitting');
       }
     });
   }
-  showAddress() {
-    const dialog = this.dialog.open(AddressBookComponent, {
-      width: '80%',
-      data: {}
-    });
-  
-    dialog.afterClosed().subscribe((result: User[]) => {
-      if (result && result.length > 0) {
-        console.log('Address Book result:', result);
-  
-        // Transform selected users
-        this.selectedUsers = result.map(user => ({
-          ...user,
-          selectedUserId: user.id
-        }));
-  
-        this.selectedToUsers = [];
-        
-        this.selectedUsers.forEach(user => {
-          if (Array.isArray(user.userStructure)) {
-            user.userStructure.forEach((u: { user: { firstname: string; lastname: string } }) => {
-              let userName = u.user.firstname + ' ' + u.user.lastname;
-              this.selectedToUsers.push({
-                structureName: user.name,
-                userName: userName
-              });
-            });
-          }
-        });
-  
-        console.log('Updating rows...');
-  
-        // Clear existing rows and add new ones for each selected user
-        this.rows = this.selectedToUsers.map(user => ({
-          ...this.createEmptyRow(),
-          selectedToUsers: [user] // Assigning the user to the row
-        }));
-  
-        console.log('Updated rows:', this.rows);
-        this.cdr.detectChanges();
-      } else {
-        console.log('Address Book dialog was closed without submitting');
-      }
-    });
-  }
-  
+
   onClose(): void {
     this.dialogRef.close();
   }
@@ -384,24 +300,22 @@ debugger
       selectedUserId: null,
       selectedPurposeId: null,
       selectedPriorityId: this.priorities.length > 0 ? this.priorities[0].id : null,
-      selectedDueDate: null as Date | null,
+      selectedDueDate: null as Date |null,
       txtInstruction: '',
       isPrivate: false,
       isCCed: false,
       isFollowUp: false,
       isStructure: false, // Default value
-      selectedUser: null as { id: number, name: string, structureId: number, isStructure: boolean } | null,
-      showValidationError: false,
-     selectedToUsers: [] as StructureUsers[] 
+      selectedUser: null as { id: number, name: string,structureId:number, isStructure: boolean } | null,
+      showValidationError: false
     };
   }
   onUserOrPurposeChange(index: number) {
     // Add a new row when user is selected, only if it's the last row
-    debugger
-    if (this.rows[index].selectedPurposeId == 10) {
+    if(this.rows[index].selectedPurposeId==10){
       this.rows = this.rows.map((row, i) =>
-        i === index ? { ...row, isCCed: true } : row
-      );
+    i === index ? { ...row, isCCed: true } : row
+  );
     }
     if (index === this.rows.length - 1) {
       this.rows.push(this.createEmptyRow());
@@ -409,65 +323,69 @@ debugger
   }
 
 
-  Transfer(): void {
-    // Validate required fields for each row efficiently
-    const rowsToValidate = this.rows.length > 1 ? this.rows.slice(0, -1) : this.rows;
-    const isValid = rowsToValidate.every((row) => {
-      const isRowValid = row.selectedUser && row.selectedPurposeId && row.selectedPriorityId;
-      row.showValidationError = !isRowValid; // Mark row as invalid if required fields are missing
-      return isRowValid;
-    });
+Transfer(): void {
+  // Validate required fields for each row efficiently
+  const rowsToValidate = this.rows.length > 1 ? this.rows.slice(0, -1) : this.rows;
+  const isValid = rowsToValidate.every((row) => {
+    const isRowValid = row.selectedUser && row.selectedPurposeId && row.selectedPriorityId;
+    row.showValidationError = !isRowValid; // Mark row as invalid if required fields are missing
+    return isRowValid;
+  });
 
-    if (!isValid) {
-      this.toaster.showToaster("Please fill all required fields before transferring.");
-      return;
-    }
+  if (!isValid) {
+    this.toaster.showToaster("Please fill all required fields before transferring.");
+    return;
+  }
 
-    const rowData = this.collectRowData();
+  const rowData = this.collectRowData();
 
-    this.mailService.transferMail(this.accessToken!, rowData).subscribe(
-      (result) => {
-        if (result.length > 0) {
-          const message = result[0].updated ? "Sent successfully" : result[0].message;
-          this.toaster.showToaster(message);
-          this.onClose();
-        }
-      },
-      (error) => {
-        this.toaster.showToaster(error.error.text ?? "Something went wrong");
+  this.mailService.transferMail(this.accessToken!, rowData).subscribe(
+    (result) => {
+      if (result.length > 0) {
+        const message = result[0].updated ? "Sent successfully" : result[0].message;
+        this.toaster.showToaster(message);
+        this.onClose();
       }
-    );
-  }
+    },
+    (error) => {
+      this.toaster.showToaster(error.error.text ?? "Something went wrong");
+    }
+  );
+}
 
-  collectRowData(): TransferRow[] {
-    const formattedDate = (date: Date | null) => date ? date.toLocaleDateString('en-GB') : null;
+collectRowData(): TransferRow[] {
+  const formattedDate = (date: Date | null) => date ? date.toLocaleDateString('en-GB') : null;
 
-    return this.rows.slice(0, -1).map(({
-      selectedUser, selectedPurposeId, selectedPriorityId, selectedDueDate,
-      txtInstruction, isPrivate, isCCed, isFollowUp , selectedToUsers
-    }) => {
-      const isStructure = selectedUser?.isStructure ?? false;
-      const userId = selectedUser?.id ?? null;
+  return this.rows.slice(0, -1).map(({ 
+    selectedUser, selectedPurposeId, selectedPriorityId, selectedDueDate, 
+    txtInstruction, isPrivate, isCCed, isFollowUp 
+  }) => {
+    const isStructure = selectedUser?.isStructure ?? false;
+    const userId = selectedUser?.id ?? null;
 
-      return {
-        purposeId: selectedPurposeId ?? null,
-        priorityId: selectedPriorityId ?? null,
-        dueDate: formattedDate(selectedDueDate),
-        instruction: txtInstruction ?? '',
-        isPrivate: isPrivate ?? false,
-        cced: isCCed ?? false,
-        followUp: isFollowUp ?? false,
-        toStructureId: isStructure ? userId : selectedUser?.structureId,
-        toUserId: !isStructure ? userId : null,
-        name: selectedUser?.name || '',
-        PrivateInstruction: false,
-        FromStructureId: this.fromStructureId ?? false,
-        ParentTransferId: this.parentTransferId ?? null,
-        IsStructure: isStructure,
-        DocumentId: this.documentId ?? null,
-        DocumentPrivacyId: this.documentPrivacyId ?? null
-      } as TransferRow;
-    });
-  }
+    return {
+      purposeId: selectedPurposeId ?? null,
+      priorityId: selectedPriorityId ?? null,
+      dueDate: formattedDate(selectedDueDate),
+      instruction: txtInstruction ?? '',
+      isPrivate: isPrivate ?? false,
+      cced: isCCed ?? false,
+      followUp: isFollowUp ?? false,
+      toStructureId: isStructure ? userId : selectedUser?.structureId,
+      toUserId: !isStructure ? userId : null,
+      name: selectedUser?.name || '',
+      PrivateInstruction: false,
+      FromStructureId: this.fromStructureId ?? false,
+      ParentTransferId: this.parentTransferId ?? null,
+      IsStructure: isStructure,
+      DocumentId: this.documentId ?? null,
+      DocumentPrivacyId: this.documentPrivacyId ?? null
+    } as TransferRow;
+  });
+}
 
+ 
+
+
+  
 }
