2c492da01aada097e5b2339207f4905f6c645441
Eyyadd <eyadmahmmoud20@gmail.com>
2025-03-09 11:07:30 +0200

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,16 @@
-// Development environment
+// // Development environment
+// export const environment = {
+//     production: false,
+//     apiBaseUrl: 'https://cts-qatar.d-intalio.com',
+//     iAMUrl: 'https://iam-qatar.d-intalio.com',
+//     viewerUrl:'https://java-qatar.d-intalio.com/VIEWER/file'
+// }; 
+
 export const environment = {
     production: false,
-    apiBaseUrl: 'https://cts-qatar.d-intalio.com',
-    iAMUrl: 'https://iam-qatar.d-intalio.com',
-    viewerUrl:'https://java-qatar.d-intalio.com/VIEWER/file'
-}; 
+    apiBaseUrl: 'http://qdbcts.d-intalio.com',
+    iAMUrl: 'http://qdbiam.d-intalio.com',
+    clientId: '648074f6-1030-413d-b700-9814626361ab',
+    clientSecret: '2a1731ec-1c3f-45ec-9a75-81034df06f89',
+    viewerUrl: 'http://qdbjava.d-intalio.com/VIEWER'
+};
