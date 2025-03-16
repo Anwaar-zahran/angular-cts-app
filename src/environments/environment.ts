67d99784574df7dfec307c62865673a832d8282c
Rana EZZAT <rana.ezzat@intalio.com>
2025-03-16 10:05:35 +0200

    commenting cookieservice

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,22 @@
 
 // // Development environment
+//export const environment = {
+//    production: false,
+//    apiBaseUrl: 'https://cts-qatar.d-intalio.com',
+//    iAMUrl: 'https://iam-qatar.d-intalio.com',
+//    clientId: '5d2c8fa5-9f58-430c-bcf2-5f4366d425dc',
+//    clientSecret: 'd85a0d00-a065-4d8e-b001-f39d69951555',
+//    viewerUrl: 'https://java-qatar.d-intalio.com/VIEWER/file',
+//    VIPClientId:'f1e4f868-87e7-4a0a-b87b-13b90e9185ae',
+//    VIPClientSecret:'d7adea43-7e21-4c34-84ce-730b8c9705e4'
+//}; 
 export const environment = {
-    production: false,
-    apiBaseUrl: 'https://cts-qatar.d-intalio.com',
-    iAMUrl: 'https://iam-qatar.d-intalio.com',
-    clientId: '5d2c8fa5-9f58-430c-bcf2-5f4366d425dc',
-    clientSecret: 'd85a0d00-a065-4d8e-b001-f39d69951555',
-    viewerUrl: 'https://java-qatar.d-intalio.com/VIEWER/file',
-    VIPClientId:'f1e4f868-87e7-4a0a-b87b-13b90e9185ae',
-    VIPClientSecret:'d7adea43-7e21-4c34-84ce-730b8c9705e4'
-}; 
-// export const environment = {
-//     production: false,
-//     apiBaseUrl: 'http://qdbcts.d-intalio.com',
-//     iAMUrl: 'http://qdbiam.d-intalio.com',
-//     clientId: '648074f6-1030-413d-b700-9814626361ab',
-//     clientSecret: '2a1731ec-1c3f-45ec-9a75-81034df06f89',
-//     viewerUrl: 'http://qdbjava.d-intalio.com/VIEWER/'
-// };
+  production: false,
+  apiBaseUrl: 'http://qdbcts.d-intalio.com',
+  iAMUrl: 'http://qdbiam.d-intalio.com',
+  clientId: '648074f6-1030-413d-b700-9814626361ab',
+  clientSecret: '2a1731ec-1c3f-45ec-9a75-81034df06f89',
+  viewerUrl: 'http://qdbjava.d-intalio.com/VIEWER/',
+  VIPClientId: 'f1e4f868-87e7-4a0a-b87b-13b90e9185ae',
+  VIPClientSecret: 'd7adea43-7e21-4c34-84ce-730b8c9705e4'
+};
