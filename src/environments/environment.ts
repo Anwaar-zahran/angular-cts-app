e00de63c53155df33283b3a0dc91daeb1457abca
Anwaar <anwaar.zahran@intalio.com>
2025-03-16 13:51:20 +0200

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,6 +17,6 @@ export const environment = {
   clientId: '648074f6-1030-413d-b700-9814626361ab',
   clientSecret: '2a1731ec-1c3f-45ec-9a75-81034df06f89',
   viewerUrl: 'http://qdbjava.d-intalio.com/VIEWER/',
-  VIPClientId: 'f1e4f868-87e7-4a0a-b87b-13b90e9185ae',
-  VIPClientSecret: 'd7adea43-7e21-4c34-84ce-730b8c9705e4'
+  VIPClientId: '56b6fb95-e42e-4787-94fd-48957a35f611',
+  VIPClientSecret: 'a84ecbad-e9f7-46e0-b365-57e47a4c5039'
 };
