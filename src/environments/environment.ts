452210741ab0942ea35447a5a687bfcd6c479dea
Anwaar <anwaar.zahran@intalio.com>
2025-04-08 12:03:54 +0200

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,4 @@
-// Development environment qatar
-//export const environment = {
-//    production: false,
-//    apiBaseUrl: 'https://cts-qatar.d-intalio.com',
-//    iAMUrl: 'https://iam-qatar.d-intalio.com',
-//    clientId: '5d2c8fa5-9f58-430c-bcf2-5f4366d425dc',
-//    clientSecret: 'd85a0d00-a065-4d8e-b001-f39d69951555',
-//    viewerUrl: 'https://java-qatar.d-intalio.com/VIEWER/file',
-//    VIPClientId:'f1e4f868-87e7-4a0a-b87b-13b90e9185ae',
-//    VIPClientSecret:'d7adea43-7e21-4c34-84ce-730b8c9705e4'
-//}; 
+
 
 // Development environment qdb
 export const environment = {
